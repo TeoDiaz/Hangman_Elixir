292ef2753df168d3f3553bf9b0efd74bde786f54
TeoDiaz <tdiazsan@gmail.com>
2019-03-06 14:40:44 +0100

    Created proyect with mix, create random word function

---
+++ Hangman/dictionary/lib/dictionary.ex
@@ -0,0 +1,26 @@
+defmodule Dictionary do
+
+  @doc """
+  def random_word() do
+    Enum.random(word_list())
+  end
+
+  def word_list do
+    contents = File.read!("assets/words.txt")
+    list = String.split(contents,  ~r/\n/)
+  end
+  """
+  #Refactor
+
+  def random_world do
+    world_list()
+    |> Enum.random()
+  end
+
+  def world_list do
+    "assets/words.txt"
+    |> File.read!()
+    |> String.split(~r/\n/)
+  end
+
+end
