8fb1799adf2f64b158650257dd374bc0b8ebe9da
TeoDiaz <tdiazsan@gmail.com>
2019-03-09 23:17:28 +0100

    Create Agent to manage state

---
+++ Hangman/dictionary/lib/application.ex
@@ -0,0 +1,4 @@
+defmodule Dictionary.Application do
+
+
+end
--- procs.ex
+++ procs.ex
@@ -2,6 +2,8 @@ defmodule Procs do
 
   def greeter(count) do
     receive do
+      {:boom, reason} ->
+        exit(reason)
       {:add, n} ->
         greeter(count+n)
       msg ->
@@ -11,7 +13,15 @@ defmodule Procs do
   end
 
   # spawn Procs, :greeter, ["world"]
+  # spawn_link Procs, :greeter, ["world"]
   # MFA is short for Module, Function, Arguments
 
+  @doc """
+  spawn creates an isolated process—it is independent of the process that created it. You don’t (by default) receive any notification that it has died.
+
+spawn_link links the creating and created processes. If one dies an abnormal death, the other is killed.
+
+Most of the time, this is the behavior we want. Without it, we’ll leave zombies lying around, an we won’t know that subprocesses have died.
+  """
 
 end
