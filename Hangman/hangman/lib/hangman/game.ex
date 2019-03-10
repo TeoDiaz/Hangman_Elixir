88eb804e47328e42019c3969fa4acf45a1d6ff7e
TeoDiaz <tdiazsan@gmail.com>
2019-03-10 02:29:20 +0100

    Created GenServer

--- Hangman/hangman/lib/hangman/game.ex
+++ Hangman/hangman/lib/hangman/game.ex
@@ -1,10 +1,14 @@
+
+
 defmodule Hangman.Game do
+
   defstruct(
     turns_left: 7,
     game_state: :initializing,
-    letters: [],
-    used: MapSet.new(),
+    letters:    [],
+    used:       MapSet.new(),
   )
+
   def new_game(word) do
     %Hangman.Game{
       letters: word |> String.codepoints
@@ -15,26 +19,30 @@ defmodule Hangman.Game do
     new_game(Dictionary.random_word)
   end
 
-  def make_move(game = %{game_state: state}, _guess) when state in [:won, :lost] do
+  def make_move(game = %{ game_state: state }, _guess) when state in [:won, :lost] do
     game
+    |> return_with_tally()
   end
 
-  def make_move(game,guess) do
-   accept_move(game, guess, MapSet.member?(game.used, guess))
+  def make_move(game, guess) do
+    accept_move(game, guess, MapSet.member?(game.used, guess))
+    |> return_with_tally()
   end
 
   def tally(game) do
     %{
       game_state: game.game_state,
       turns_left: game.turns_left,
-      letters: game.letters |> reveal_guessed(game.used)
+      letters:    game.letters |> reveal_guessed(game.used),
+      used:       game.used |> MapSet.to_list |> Enum.sort
     }
   end
 
-#########################################################################
+
+  ############################################################
 
 
-  defp accept_move(game, guess, _already_guessed = true) do
+  defp accept_move(game, _guess, _already_guessed = true) do
     Map.put(game, :game_state, :already_used)
   end
 
@@ -43,31 +51,36 @@ defmodule Hangman.Game do
     |> score_guess(Enum.member?(game.letters, guess))
   end
 
-  defp score_guess(game,_good_guess = true) do
+  defp score_guess(game, _good_guess = true) do
     new_state = MapSet.new(game.letters)
     |> MapSet.subset?(game.used)
     |> maybe_won()
     Map.put(game, :game_state, new_state)
   end
 
-  defp score_guess(game = %{turns_left: 1}, _not_good_guess) do
+  defp score_guess(game = %{ turns_left: 1}, _not_good_guess) do
     Map.put(game, :game_state, :lost)
   end
 
-  defp score_guess(game = %{turns_left: turns_left}, _not_good_guess) do
-    %{game | game_state: :bad_guess, turns_left: turns_left - 1}
+  defp score_guess(game = %{ turns_left: turns_left}, _not_good_guess) do
+    %{ game |
+       game_state: :bad_guess,
+       turns_left: turns_left - 1
+    }
   end
+
 
   defp reveal_guessed(letters, used) do
     letters
-    |> Enum.map(fn letter -> reveal_letter(letter, MapSet.member?(used, letter))  end)
+    |> Enum.map(fn letter -> reveal_letter(letter, MapSet.member?(used, letter)) end)
   end
 
-  defp reveal_letter(letter, _in_word = true), do: letter
-  defp reveal_letter(letter, _not_in_), do: "_"
+  defp reveal_letter(letter, _in_word = true),  do: letter
+  defp reveal_letter(_letter, _not_in_word),    do: "_"
 
   defp maybe_won(true), do: :won
-  defp maybe_won(_), do: :good_guess
+  defp maybe_won(_),    do: :good_guess
 
+  defp return_with_tally(game), do: { game, tally(game) }
 
 end
--- Hangman/hangman/test/game_test.exs
+++ Hangman/hangman/test/game_test.exs
@@ -13,42 +13,42 @@ defmodule GameTest do
   test "state isn't changed for :won or :lost game" do
     for state <- [:won, :lost] do
     game = Game.new_game() |> Map.put(:game_state, state)
-    assert ^game = Game.make_move(game, "x")
+    assert {^game,_} = Game.make_move(game, "x")
     end
   end
 
   test "first ocurrence of letter is not already used" do
     game = Game.new_game()
-    game = Game.make_move(game, "x")
+    {game, tally} = Game.make_move(game, "x")
     assert game.game_state != :already_used
   end
 
 
   test "a good guess is recognized" do
     game = Game.new_game("cabify")
-    game = Game.make_move(game, "c")
+    {game, tally} = Game.make_move(game, "c")
     assert game.game_state == :good_guess
     assert game.turns_left == 7
   end
 
   test "a guessed word is a won game" do
     game = Game.new_game("cabify")
-    game = Game.make_move(game, "c")
+    {game, tally} = Game.make_move(game, "c")
     assert game.game_state == :good_guess
     assert game.turns_left == 7
-    game = Game.make_move(game, "a")
+    {game, tally} = Game.make_move(game, "a")
     assert game.game_state == :good_guess
     assert game.turns_left == 7
-    game = Game.make_move(game, "b")
+    {game, tally} = Game.make_move(game, "b")
     assert game.game_state == :good_guess
     assert game.turns_left == 7
-    game = Game.make_move(game, "i")
+    {game, tally} = Game.make_move(game, "i")
     assert game.game_state == :good_guess
     assert game.turns_left == 7
-    game = Game.make_move(game, "f")
+    {game, tally} = Game.make_move(game, "f")
     assert game.game_state == :good_guess
     assert game.turns_left == 7
-    game = Game.make_move(game, "y")
+    {game, tally} = Game.make_move(game, "y")
     assert game.game_state == :won
     assert game.turns_left == 7
   end
@@ -56,32 +56,32 @@ defmodule GameTest do
 
   test "a bad guess is recognized" do
     game = Game.new_game("cabify")
-    game = Game.make_move(game, "z")
+    {game, tally} = Game.make_move(game, "z")
     assert game.game_state == :bad_guess
     assert game.turns_left == 6
   end
 
   test "a lost game is recognized" do
     game = Game.new_game("cabify")
-    game = Game.make_move(game, "z")
+    {game, tally} = Game.make_move(game, "z")
     assert game.game_state == :bad_guess
     assert game.turns_left == 6
-    game = Game.make_move(game, "e")
+    {game, tally} = Game.make_move(game, "e")
     assert game.game_state == :bad_guess
     assert game.turns_left == 5
-    game = Game.make_move(game, "x")
+    {game, tally} = Game.make_move(game, "x")
     assert game.game_state == :bad_guess
     assert game.turns_left == 4
-    game = Game.make_move(game, "o")
+    {game, tally} = Game.make_move(game, "o")
     assert game.game_state == :bad_guess
     assert game.turns_left == 3
-    game = Game.make_move(game, "r")
+    {game, tally} = Game.make_move(game, "r")
     assert game.game_state == :bad_guess
     assert game.turns_left == 2
-    game = Game.make_move(game, "p")
+    {game, tally} = Game.make_move(game, "p")
     assert game.game_state == :bad_guess
     assert game.turns_left == 1
-    game = Game.make_move(game, "t")
+    {game, tally} = Game.make_move(game, "t")
     assert game.game_state == :lost
   end
 
