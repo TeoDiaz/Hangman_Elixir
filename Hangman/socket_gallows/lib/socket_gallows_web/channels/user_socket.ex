38acd6ffc30740a959119344c9758940c2e0db20
TeoDiaz <tdiazsan@gmail.com>
2019-03-11 12:53:29 +0100

    Connected Socket

--- Hangman/socket_gallows/lib/socket_gallows_web/channels/user_socket.ex
+++ Hangman/socket_gallows/lib/socket_gallows_web/channels/user_socket.ex
@@ -2,7 +2,7 @@ defmodule SocketGallowsWeb.UserSocket do
   use Phoenix.Socket
 
   ## Channels
-  channel "room:*", SocketGallowsWeb.HangmanChannel
+  channel "hangman:*", SocketGallowsWeb.HangmanChannel
 
   def connect(_params, socket, _connect_info) do
     {:ok, socket}
