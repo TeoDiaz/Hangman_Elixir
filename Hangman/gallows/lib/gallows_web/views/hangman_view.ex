f16e22d0eebd8d7dc98e02ff4ed0ab2f1333b641
TeoDiaz <tdiazsan@gmail.com>
2019-03-11 11:12:45 +0100

    Create socket to connect

--- Hangman/gallows/lib/gallows_web/views/hangman_view.ex
+++ Hangman/gallows/lib/gallows_web/views/hangman_view.ex
@@ -1,6 +1,15 @@
 defmodule GallowsWeb.HangmanView do
   use GallowsWeb, :view
+
   import Gallows.Views.Helpers.GameStateHelper
+
+  def turn(left, target) when target >= left do
+    "opacity: 1"
+  end
+
+  def turn(left, target) do
+    "opacity: 0.1"
+  end
 
   def game_over?(%{game_state: game_state}) do
     game_state in  [:won, :lost]
--- Hangman/socket_gallows/mix.exs
+++ Hangman/socket_gallows/mix.exs
@@ -1,9 +1,9 @@
-defmodule Gallows.MixProject do
+defmodule SocketGallows.MixProject do
   use Mix.Project
 
   def project do
     [
-      app: :gallows,
+      app: :socket_gallows,
       version: "0.1.0",
       elixir: "~> 1.5",
       elixirc_paths: elixirc_paths(Mix.env()),
@@ -13,16 +13,23 @@ defmodule Gallows.MixProject do
     ]
   end
 
+  # Configuration for the OTP application.
+  #
+  # Type `mix help compile.app` for more information.
   def application do
     [
-      mod: {Gallows.Application, []},
+      mod: {SocketGallows.Application, []},
       extra_applications: [:logger, :runtime_tools]
     ]
   end
 
+  # Specifies which paths to compile per environment.
   defp elixirc_paths(:test), do: ["lib", "test/support"]
   defp elixirc_paths(_), do: ["lib"]
 
+  # Specifies your project dependencies.
+  #
+  # Type `mix help deps` for examples and options.
   defp deps do
     [
       {:phoenix, "~> 1.4.1"},
@@ -31,9 +38,7 @@ defmodule Gallows.MixProject do
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"},
-
-      {:hangman, [path: "../Hangman/hangman"]}
+      {:plug_cowboy, "~> 2.0"}
     ]
   end
 end
